493c27f41b24a095072fe7a17cc4ae48c61e2032
mitsururike <mitsururike2@gmail.com>
2017-12-25 15:33:17 +0900

    [bugfix] fix third party repo, bump rubocop and QA (#1)
    * [bugfix] fix thirdparty repo, bump rubocop and QA
    
    * fix the failed previous commit
    
    * [bugfix] fix .kitchen.yml and directory hierarchy
    
    * [bugfix] fix travisci
    
    * QA

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,30 +1,30 @@
-require 'spec_helper'
-require 'serverspec'
+require "spec_helper"
+require "serverspec"
 
-package = 'hbase'
-service = 'hbase_regionserver'
-config  = '/etc/hbase/hbase.conf'
-user    = 'hbase'
-group   = 'hbase'
-ports   = [ 16020 ]
-log_dir = '/var/log/hbase'
-db_dir  = '/var/lib/hbase'
-config_dir = '/etc/hbase'
-env_config = '/etc/hbase/hbase-env.sh'
+package = "hbase"
+service = "hbase_regionserver"
+config  = "/etc/hbase/hbase.conf"
+user    = "hbase"
+group   = "hbase"
+ports   = [16_020]
+log_dir = "/var/log/hbase"
+db_dir  = "/var/lib/hbase"
+config_dir = "/etc/hbase"
+env_config = "/etc/hbase/hbase-env.sh"
 
 case os[:family]
-when 'freebsd'
-  config_dir = '/usr/local/etc/hbase'
-  config = '/usr/local/etc/hbase/hbase-site.xml'
-  env_config = '/usr/local/etc/hbase/hbase-env.sh'
-  db_dir = '/var/db/hbase'
+when "freebsd"
+  config_dir = "/usr/local/etc/hbase"
+  config = "/usr/local/etc/hbase/hbase-site.xml"
+  env_config = "/usr/local/etc/hbase/hbase-env.sh"
+  db_dir = "/var/db/hbase"
 end
 
 regionservers_file = "#{config_dir}/regionservers"
 
 describe package(package) do
   it { should be_installed }
-end 
+end
 
 describe file(env_config) do
   it { should be_file }
@@ -32,15 +32,15 @@ describe file(env_config) do
   its(:content) { should match Regexp.escape('export HBASE_OPTS="-XX:+UseConcMarkSweepGC"') }
   its(:content) { should match Regexp.escape('export HBASE_MASTER_OPTS="$HBASE_MASTER_OPTS -XX:PermSize=128m -XX:MaxPermSize=128m"') }
   its(:content) { should match Regexp.escape('export HBASE_REGIONSERVER_OPTS="$HBASE_REGIONSERVER_OPTS -XX:PermSize=128m -XX:MaxPermSize=128m"') }
-  its(:content) { should match Regexp.escape('export HBASE_MANAGES_ZK=false') }
+  its(:content) { should match Regexp.escape("export HBASE_MANAGES_ZK=false") }
 end
 
 describe file(config) do
   it { should be_file }
-  its(:content) { should match Regexp.escape('<name>hbase.rootdir</name>') }
-  its(:content) { should match Regexp.escape('<value>file:///var/db/hbase</value>') }
-  its(:content) { should match Regexp.escape('<name>hbase.zookeeper.property.dataDir</name>') }
-  its(:content) { should match Regexp.escape('<value>/var/db/zookeeper</value>') }
+  its(:content) { should match Regexp.escape("<name>hbase.rootdir</name>") }
+  its(:content) { should match Regexp.escape("<value>file:///var/db/hbase</value>") }
+  its(:content) { should match Regexp.escape("<name>hbase.zookeeper.property.dataDir</name>") }
+  its(:content) { should match Regexp.escape("<value>/var/db/zookeeper</value>") }
 end
 
 describe file(log_dir) do
@@ -59,15 +59,15 @@ end
 
 describe file(regionservers_file) do
   it { should be_file }
-  its(:content) { should match /127\.0\.0\.1/ }
+  its(:content) { should match(/127\.0\.0\.1/) }
 end
 
-#case os[:family]
-#when 'freebsd'
+# case os[:family]
+# when 'freebsd'
 #  describe file('/etc/rc.conf.d/hbase') do
 #    it { should be_file }
 #  end
-#end
+# end
 
 describe service(service) do
   it { should be_running }
